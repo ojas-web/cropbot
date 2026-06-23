6f6176bf11a691c61a262493a20cbe014478934b
ojas-web <ojasg9757@gmail.com>
2026-06-23 23:15:44 +0530

    Refactor server log message to single line

--- server.js
+++ server.js
@@ -87,11 +87,7 @@ try {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
-
-```
-console.log(
-    `Server running on port ${PORT}`
-);
-```
+    console.log(`Server running on port ${PORT}`);
+});
 
 });
